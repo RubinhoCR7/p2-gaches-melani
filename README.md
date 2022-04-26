378351a14ce4a68e5918b4da9ebd9231127b5040
Ruben Gaches <ruben@MacBook-Pro-de-Ruben.local>
2022-04-26 13:13:23 +0200

    ajout de la base de données en sql

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 todolist
 - get x 2 (check x2)
 - post x 1
-- put
-- delete X 1 
+- put X 1
+- delete X 1
 
 - code sql pour créer la bdd (check)
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mysql = require('./database');
 const app = express();
 
 //je définie le port sur lequel va se créer mon serveur
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 // j'affiche dans ma console le port pour valider
 
@@ -14,7 +14,7 @@ app.listen(PORT, () => {
 
 mysql.connect((err)=>{
     if(!err)
-    console.log('DB connect');
+    console.log('DB preconnect');
     else
     console.log('DB not connect \n Error : ' + JSON.stringify(err, undefined, 2));
 })
